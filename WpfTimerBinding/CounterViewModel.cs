41ce2c64d9383b17d6e9e9717bbf3f98657025e1
cmwoo <woochang0110@gmail.com>
2025-08-16 01:28:29 +0900

    [우창민] Timer와 데이터 바인딩을 통한 GUI 갱신 프로그램

---
+++ WpfTimerBinding/CounterViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfTimerBinding
+{
+    public class CounterViewModel : INotifyPropertyChanged
+    {
+        private int _number;
+        public int Number
+        {
+            get => _number;
+            set
+            {
+                if (_number != value)
+                {
+                    _number = value;
+                    OnPropertyChanged(nameof(Number));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private CancellationTokenSource _cts;
+
+        public void StartCounter()
+        {
+            _cts = new CancellationTokenSource();
+            var token = _cts.Token;
+
+            Task.Run(async () =>
+            {
+                for (int i = 1; i <= 1000; i++)
+                {
+                    if (token.IsCancellationRequested)
+                        break;
+
+                    Number = i; // UI는 Dispatcher가 알아서 업데이트
+                    await Task.Delay(1000); // 0.1초마다 증가
+                }
+            }, token);
+        }
+
+        public void StopCounter()
+        {
+            _cts?.Cancel();
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfTimerBinding/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTimerBinding
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CounterViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new CounterViewModel();
+            DataContext = _viewModel;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.StartCounter();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.StopCounter();
+        }
+    }
+}
